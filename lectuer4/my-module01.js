e162428bb3b50c21774661cf77981820b34f5943
Kana123123 <ppat112233123@mail.com>
2025-06-05 11:34:34 +0700

    เพิ่ม Lecture 4 เรื่อง common Js

---
+++ lectuer4/my-module01.js
@@ -0,0 +1,14 @@
+let dataX = 'Hello...'
+
+const SAU = 'Southeast Asia University'
+
+function showWOW() {
+    console.log('Wow wow wow')
+}
+
+//เป็นการสร้าง Module แบบ CommonJS
+module.exports = {
+    dataX,
+    SAU,
+    showWOW
+}
